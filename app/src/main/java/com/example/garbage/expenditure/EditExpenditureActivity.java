54e303e8bbee27f8becd8b770e1d1ca359e7d3ac
v.matveev <iparkin.47@gmail.com>
2017-10-10 23:52:06 +0300

    Добавлена возможность наложения фильтра по дате на список операций по статье расхода

--- app/src/main/java/com/example/garbage/expenditure/EditExpenditureActivity.java
+++ app/src/main/java/com/example/garbage/expenditure/EditExpenditureActivity.java
@@ -1,5 +1,6 @@
 package com.example.garbage.expenditure;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -8,7 +9,10 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.example.garbage.IWallet;
 import com.example.garbage.R;
@@ -17,10 +21,14 @@ import com.example.garbage.cost_item.CostItemDao;
 import com.example.garbage.cost_item.CostItemsAdapter;
 import com.example.garbage.tools.GarbageTools;
 import com.example.garbage.wallet.WalletsDao;
+import com.example.garbage.wallet_operation.WalletOperationAdapter;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 public class EditExpenditureActivity extends AppCompatActivity {
@@ -38,10 +46,22 @@ public class EditExpenditureActivity extends AppCompatActivity {
     private RecyclerView.Adapter adapterCostItems;
     private RecyclerView.LayoutManager layoutManagerCostItems;
 
+    private TextView tvFromDate;
+    private TextView tvToDate;
+    private DatePickerDialog fromDatePickerDialog;
+    private DatePickerDialog toDatePickerDialog;
+    private Calendar fromDateCalendar;
+    private Calendar toDateCalendar;
+    private SimpleDateFormat dateFormat;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_expenditure);
+
+        dateFormat = new SimpleDateFormat(getString(R.string.date_label_format), Locale.getDefault());
+
+        initFromToDate();
 
         Intent intent = getIntent();
         int expenditureId = intent.getIntExtra("expenditureId", 0);
@@ -52,11 +72,10 @@ public class EditExpenditureActivity extends AppCompatActivity {
 
         costItemDao = new CostItemDao(this);
         costItems = costItemDao.getCostItems(
-                expenditureId,
+                expenditure.getId(),
                 wallets,
-                GarbageTools.getCurrentMonthStartTime(),
-                null
-        );
+                fromDateCalendar.getTimeInMillis(),
+                toDateCalendar.getTimeInMillis());
 
         etExpenditureName = (EditText) findViewById(R.id.et_edit_expenditure_name);
         etExpenditureName.setText(expenditure.getName());
@@ -68,6 +87,35 @@ public class EditExpenditureActivity extends AppCompatActivity {
         bDeleteExpenditure.setOnClickListener(getDeleteOnClickListener(this));
 
         initRecyclerViewCostItems();
+    }
+
+    private void initFromToDate() {
+        fromDateCalendar = GarbageTools.getCurrentMonthStartCalendar();
+        fromDatePickerDialog = new DatePickerDialog(
+                this,
+                getOnFromDateSetListener(),
+                fromDateCalendar.get(Calendar.YEAR),
+                fromDateCalendar.get(Calendar.MONTH),
+                fromDateCalendar.get(Calendar.DAY_OF_MONTH)
+        );
+        tvFromDate = (TextView) findViewById(R.id.tv_edit_expenditure_from_date);
+        tvFromDate.setText(dateFormat.format(fromDateCalendar.getTime()));
+        tvFromDate.setOnClickListener(getSelectFromDateOnClickListener());
+
+        toDateCalendar = Calendar.getInstance();
+        toDatePickerDialog = new DatePickerDialog(
+                this,
+                getOnToDateSetListener(),
+                toDateCalendar.get(Calendar.YEAR),
+                toDateCalendar.get(Calendar.MONTH),
+                toDateCalendar.get(Calendar.DAY_OF_MONTH)
+        );
+        tvToDate = (TextView) findViewById(R.id.tv_edit_expenditure_to_date);
+        tvToDate.setText(dateFormat.format(toDateCalendar.getTime()));
+        tvToDate.setOnClickListener(getSelectToDateOnClickListener());
+
+        ImageButton ibRefreshOperation = (ImageButton) findViewById(R.id.ib_expenditure_refresh_from_to);
+        ibRefreshOperation.setOnClickListener(getOnRefreshClickListener(this));
     }
 
     private void initRecyclerViewCostItems() {
@@ -104,6 +152,59 @@ public class EditExpenditureActivity extends AppCompatActivity {
                     setResult(RESULT_OK, new Intent());
                     finish();
                 }
+            }
+        };
+    }
+
+    private DatePickerDialog.OnDateSetListener getOnFromDateSetListener() {
+        return new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                fromDateCalendar.set(year, month, dayOfMonth);
+                tvFromDate.setText(dateFormat.format(fromDateCalendar.getTime()));
+            }
+        };
+    }
+
+    private View.OnClickListener getSelectFromDateOnClickListener() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fromDatePickerDialog.show();
+            }
+        };
+    }
+
+    private DatePickerDialog.OnDateSetListener getOnToDateSetListener() {
+        return new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                toDateCalendar.set(year, month, dayOfMonth, 23, 59, 59);
+                tvToDate.setText(dateFormat.format(toDateCalendar.getTime()));
+            }
+        };
+    }
+
+    private View.OnClickListener getSelectToDateOnClickListener() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toDatePickerDialog.show();
+            }
+        };
+    }
+
+    private View.OnClickListener getOnRefreshClickListener(Context context) {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                costItems = costItemDao.getCostItems(
+                        expenditure.getId(),
+                        wallets,
+                        fromDateCalendar.getTimeInMillis(),
+                        toDateCalendar.getTimeInMillis());
+                adapterCostItems = new CostItemsAdapter(costItems, wallets);
+                rvCostItems.setAdapter(adapterCostItems);
             }
         };
     }
