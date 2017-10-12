4a949bbe1e8d2cce992c3fd58148eca5d22f6104
v.matveev <iparkin.47@gmail.com>
2017-10-12 23:43:16 +0300

    Понижение minSdkVersion до 23

--- app/src/main/java/com/example/garbage/MainActivity.java
+++ app/src/main/java/com/example/garbage/MainActivity.java
@@ -2,10 +2,6 @@ package com.example.garbage;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.ShortcutInfo;
-import android.content.pm.ShortcutManager;
-import android.graphics.drawable.Icon;
-import android.net.Uri;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -13,9 +9,6 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.text.SpannableStringBuilder;
-import android.text.Spanned;
-import android.text.style.ForegroundColorSpan;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
@@ -33,8 +26,6 @@ import com.example.garbage.income_item.AddIncomeItemActivity;
 import com.example.garbage.income_item.EditIncomeItemActivity;
 import com.example.garbage.income_item.IncomeItem;
 import com.example.garbage.income_item.IncomeItemsDao;
-import com.example.garbage.shortcut.ShortcutOneActivity;
-import com.example.garbage.shortcut.ShortcutThreeActivity;
 import com.example.garbage.tools.GarbageTools;
 import com.example.garbage.wallet.AddWalletActivity;
 import com.example.garbage.wallet.EditWalletActivity;
@@ -42,7 +33,6 @@ import com.example.garbage.wallet.Wallet;
 import com.example.garbage.wallet.WalletsDao;
 import com.example.garbage.wallet_operation.AddWalletOperationActivity;
 
-import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -99,21 +89,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if (id == R.id.nav_shortcut_one) {
-            Intent intentShortcut = new Intent(this, ShortcutOneActivity.class);
-            startActivity(intentShortcut);
-        } else if (id == R.id.nav_preferences) {
+        if (id == R.id.nav_preferences) {
             Intent intentPref = new Intent(this, PreferencesActivity.class);
             startActivityForResult(intentPref, 1);
-        } else if (id == R.id.change_shortcut_rank) {
-            ShortcutInfo webShortcutInfo = new ShortcutInfo.Builder(this, "shortcut_two_web")
-                    .setRank(1)
-                    .build();
-            ShortcutInfo threeShortcutInfo = new ShortcutInfo.Builder(this, "shortcut_three")
-                    .setRank(0)
-                    .build();
-            ShortcutManager shortcutManager = getSystemService(ShortcutManager.class);
-            shortcutManager.updateShortcuts(Arrays.asList(webShortcutInfo, threeShortcutInfo));
         } else if (id == R.id.nav_sql) {
             Intent intentPref = new Intent(this, SQLiteActivity.class);
             startActivityForResult(intentPref, SQL_ACTIVITY_CODE);
@@ -141,7 +119,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 GarbageTools.getCurrentMonthStartTime(),
                 null);
 
-        createShortcuts();
         initNavigation();
         initFromTo();
 
@@ -213,32 +190,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         selectedWallet.updateWallet(null);
         selectedExpenditure.updateExpenditure(null);
         redrawFromTo();
-    }
-
-    private void createShortcuts() {
-        ForegroundColorSpan colorSpan = new ForegroundColorSpan(getResources().getColor(android.R.color.holo_blue_light, getTheme()));
-        String label = "Динамический web";
-        SpannableStringBuilder colouredLabel = new SpannableStringBuilder(label);
-        colouredLabel.setSpan(colorSpan, 0, label.length(), Spanned.SPAN_INCLUSIVE_INCLUSIVE);
-
-        ShortcutInfo webShortcutInfo = new ShortcutInfo.Builder(this, "shortcut_two_web")
-                .setShortLabel(colouredLabel)
-                .setLongLabel("Открываем сайт google.com")
-                .setIcon(Icon.createWithResource(this, R.drawable.shortcut_icon_2))
-                .setIntent(new Intent(Intent.ACTION_VIEW, Uri.parse("https://google.com")))
-                .build();
-
-        ShortcutInfo threeShortcutInfo = new ShortcutInfo.Builder(this, "shortcut_three")
-                .setShortLabel("Динамический")
-                .setLongLabel("Открываем динамически")
-                .setIcon(Icon.createWithResource(this, R.drawable.shortcut_icon_3))
-                .setIntents(new Intent[]{
-                        new Intent(Intent.ACTION_MAIN, Uri.EMPTY, this, MainActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK),
-                        new Intent(Intent.ACTION_MAIN, Uri.EMPTY, this, ShortcutThreeActivity.class)})
-                .build();
-
-        ShortcutManager shortcutManager = getSystemService(ShortcutManager.class);
-        shortcutManager.setDynamicShortcuts(Arrays.asList(webShortcutInfo, threeShortcutInfo));
     }
 
     private void initNavigation() {
